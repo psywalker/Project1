e8d97ae563a973134b37d0b4fa32f44593015af3
psywalker <psywalker09@gmail.com>
2018-10-13 15:58:42 +0300

    Изменения4

---
+++ dist/main.js
@@ -0,0 +1,21 @@
+webpackJsonp([1],{
+
+/***/ 9:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__Hello_vue__ = __webpack_require__(3);
+
+
+
+new __WEBPACK_IMPORTED_MODULE_0_vue__["default"]({
+  el: '#app',
+  render: h => h(__WEBPACK_IMPORTED_MODULE_1__Hello_vue__["default"])
+});
+
+
+/***/ })
+
+},[9]);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Hello from './Hello.vue'
+import './style.scss';
 
 new Vue({
   el: '#app',
