ac05d27d1f0400f880d1fe7df96d8c36d1a90659
psywalker <psywalker09@gmail.com>
2018-10-13 12:50:32 +0300

    Изменения2

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
         options: {
           loaders: {}
         }
+      }, 
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
       }
     ]
   },
